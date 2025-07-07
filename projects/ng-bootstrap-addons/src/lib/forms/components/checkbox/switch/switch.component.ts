9bec15faf5e7e21cf888c56748bc5c445021ce9b
Mikael <mbotassi@elfsm.com.br>
2025-07-07 11:36:56 -0300

    feat: add switch component with template and control value accessor

---
+++ projects/ng-bootstrap-addons/src/lib/forms/components/checkbox/switch/switch.component.ts
@@ -0,0 +1,18 @@
+import { Component, forwardRef } from '@angular/core';
+import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { ControlValueAccessorDirective } from '../../../directives/control-value-acessor.directive';
+
+@Component({
+  selector: 'nba-switch',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './switch.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SwitchComponent),
+      multi: true,
+    },
+  ],
+})
+export class SwitchComponent<T> extends ControlValueAccessorDirective<T> {
+}
