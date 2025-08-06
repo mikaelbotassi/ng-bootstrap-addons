e256a4785de3c255a32e8004a6e1d5aabd7e86ab
Mikael <mbotassi@elfsm.com.br>
2025-08-06 18:04:40 -0300

    chore: Updated Version to Angular 20

--- projects/demo/src/app/components/ac-search-sample/ac-search-sample.component.ts
+++ projects/demo/src/app/components/ac-search-sample/ac-search-sample.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, OnInit, signal } from '@angular/core';
 import { SampleContainerComponent } from '../../containers/sample-container/sample-container.component';
 import { AutoCompleteLovComponent, acMap } from 'inputs/ac-search-lov/ac-search-lov.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -8,15 +8,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [SampleContainerComponent, AutoCompleteLovComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './ac-search-sample.component.html',
 })
-export class AcSearchSampleComponent implements AfterViewInit {
+export class AcSearchSampleComponent implements OnInit {
   mapConfig: acMap = {
     code: { key: 'id', title: 'ID' },
     desc: { key: 'name', title: 'Name' }
   }
-  selectedPokemon:number|null = null;
+  selectedPokemon = signal<number|null>(null);
 
-  ngAfterViewInit(): void {
-    this.selectedPokemon = 5;
+  ngOnInit(): void {
+    this.selectedPokemon.set(5);
   }
 
 }
--- projects/ng-bootstrap-addons/label/src/label.component.ts
+++ projects/ng-bootstrap-addons/label/src/label.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { booleanAttribute, Component, input, ViewEncapsulation } from '@angular/core';
+import { createRandomString } from 'ng-bootstrap-addons/utils';
 
 @Component({
   selector: 'nba-label',
@@ -7,6 +8,7 @@ import { booleanAttribute, Component, input, ViewEncapsulation } from '@angular/
   templateUrl: './label.component.html',
   styleUrls: ['./label.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  host: { 'collision-id': `ac-search-sample-${createRandomString(20)} ` },
 })
 export class LabelComponent {
   required = input(false, {transform: booleanAttribute});
